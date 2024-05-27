48c57a76bc280008f95f67d64bd729fcda5b6dbf
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-05-27 18:12:59 +0000

    done

--- js/script.js
+++ js/script.js
@@ -5,30 +5,30 @@
 // This file contains the JS functions for index.html
 
 
-"use strict";
+"use strict"
 
 function multiplyNumbers() {
 
-  const firstNumber = parseInt(document.getElementById('firstNumberInput').value);
-  let secondNumber = parseInt(document.getElementById('secondNumberInput').value);
+  const firstNumber = parseInt(document.getElementById('firstNumberInput').value)
+  let secondNumber = parseInt(document.getElementById('secondNumberInput').value)
 
-  let result = 0;
-  let counter = 0;
+  let result = 0
+  let counter = 0
 
-  let isNegative = secondNumber < 0;
-  if (isNegative === true) {
+  let isNegative = secondNumber < 0
+  if (isNegative == true) {
     secondNumber = -secondNumber;
   }
 
   while (counter < secondNumber) {
-    result += firstNumber;
-    counter++;
+    result += firstNumber
+    counter++
   }
 
-  if (isNegative === true) {
-    result = -result;
+  if (isNegative == true) {
+    result = -result
   }
 
-  document.getElementById("multiplicationResult").innerHTML = "<p>" + firstNumber + " x " + secondNumber + " = " + result + "</p>";
+  document.getElementById("multiplicationResult").innerHTML = "<p>" + firstNumber + " x " + secondNumber + " = " + result + "</p>"
 }
 
